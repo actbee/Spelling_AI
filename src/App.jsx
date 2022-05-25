91ff9371574a84cba4b3dcc975c941e97fffbc47
actbee <rainman511@outlook.com>
2022-05-24 21:57:56 -0400

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import {Route, Routes} from "react-router-dom";
 import Main from "./components/main/main"
-import Data from "./components/data/data"
 import Error from "./components/error"
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
     <div className="App">
       <Routes>
         <Route path= "/" element = {<Main />} exact/>
-        <Route path = "/data" element = {<Data />} exact></Route>
         <Route element = {<Error />} />
       </Routes>
     </div>
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -3,86 +3,77 @@ import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import { Link } from "react-router-dom";
 import Box from '@mui/material/Box';
+
+import Slider from '@mui/material/Slider';
 import TextField from '@mui/material/TextField';
-import {useState} from "react";
+import {useState, useRef} from "react";
+import {levenshteinDistance} from "../distance/distance"
+
 
 export default function Main(){
     const [check, setcheck] = useState(false);
+    const [incorrect, setincorrect] = useState(false);
+    const [test, settest] = useState("NA");
+    const [check_result, setcheckresult] = useState(" ");
     var dict = {}
     const matchlist = [{word: "aaa", dis: 0.0},{word: "baa", dis: 0.1},{word: "caa", dis: 1.0},{word: "ada", dis: 3.0},
-    {word: "affa", dis: 5.0},{word: "aweaa", dis: 5.5},{word: "aa12a", dis: 6.0},{word: "aaera", dis: 7.0},
-    {word: "aavva", dis: 7.8},{word: "ddaaa", dis: 8.0}];
+    {word: "affa", dis: 5.0}];
+    const predictRef = useRef(" ")
+    const dicRef = useRef(" ")
+
     
+
+
     const check_click = () => {
        // if input matches any of the words in the list, then print this word is spelled correctly
-        
-        var spellcheck_input = document.getElementById("spellcheck_input").value;
+       settest(predictRef.current.value);
+        setcheck(true);
+        var spellcheck_input = test;
+        console.log(spellcheck_input);
         var incorrect = true;
         for (var value in dict) {
             if (dict[value].toLowerCase() == spellcheck_input.toLowerCase()) {
-                console.log("this word is spelled correctly");
+                setcheckresult("this word is spelled correctly");
                 incorrect = false;
+                setincorrect(false);
                 break;
             }
         }        
         if (incorrect) {
-            console.log("this word is spelled incorrectly");
-        }
-        // if (incorrect) {
-        //   const levenshteinDistance = (str1 = '', str2 = '') => {
-        //     const track = Array(str2.length + 1).fill(null).map(() =>
-        //     Array(str1.length + 1).fill(null));
-        //     for (let i = 0; i <= str1.length; i += 1) {
-        //        track[0][i] = i;
-        //     }
-        //     for (let j = 0; j <= str2.length; j += 1) {
-        //        track[j][0] = j;
-        //     }
-        //     for (let j = 1; j <= str2.length; j += 1) {
-        //        for (let i = 1; i <= str1.length; i += 1) {
-        //           const indicator = str1[i - 1] === str2[j - 1] ? 0 : 1;
-        //           track[j][i] = Math.min(
-        //              track[j][i - 1] + 1, // deletion
-        //              track[j - 1][i] + 1, // insertion
-        //              track[j - 1][i - 1] + indicator, // substitution
-        //           );
-        //        }
-        //     }
-        //     return track[str2.length][str1.length];
-        //  };
-        //   // computes a suggestionScore for each word in the dictionary by levenshtein distance and word frequency
-        //   var suggestionScore = [];
-        //   for (var j = 0; j < dict.length; j++) {
-        //     var word = dict[j];
-        //     var distance = levenshteinDistance(spellcheck_input, word);
-        //     var score = dict[j].dis;
-        //     suggestionScore.push(distance + score);
-        //   }
-        //   // sorts the suggestionScore array by suggestionScore
-        //   suggestionScore.sort(function(a, b) {
-        //     return a.dis - b.dis;
-        //   }
-        //   );
-        //   // prints the top 5 suggestions
-        //   console.log("suggestions: ");
-        //   for (var i = 0; i < 5; i++) {
-        //     console.log(suggestionScore[i]);
-        //   }
+            setcheckresult("this word is spelled incorrectly");
+            setincorrect(true);
+           // computes a suggestionScore for each word in the dictionary by levenshtein distance and word frequency
+           var suggestionScore = [];
+           for (var j = 0; j < dict.length; j++) {
+             var word = dict[j];
+             var distance = levenshteinDistance(spellcheck_input, word);
+             var score = dict[j].dis;
+             suggestionScore.push(distance + score);
+           }
+           // sorts the suggestionScore array by suggestionScore
+           suggestionScore.sort(function(a, b) {
+             return a.dis - b.dis;
+           }
+           );
+           // prints the top 5 suggestions
+           console.log("suggestions: ");
+           for (var i = 0; i < 5; i++) {
+             console.log(suggestionScore[i]);
+           }
 
-        // }
+         }
     }
 
     const savedict = () => {
       // put the user input from id training_text into dict
-      
-      var text = document.getElementById("training_text").value;
+      var text = dicRef.current.value;
       var words = text.split(" ");
       // put words into dict
       for (let i = 0; i < words.length; i++){
         dict[i] = words[i];
       }
       console.log("updated dict!");
-      
+      console.log(dict);
     }
 
     return(
@@ -102,17 +93,40 @@ export default function Main(){
                     inputProps = {{style: {fontSize: 20}}}
                     fullWidth 
                     margin = "normal"
+                    inputRef={predictRef}
                  />
                 <Box m={2}>
                 <Button margin="20px" size = "large" variant = "contained"  onClick = {check_click}>Check Spelling</Button>
                 </Box>
+                <p>Levenshtein Distance Scalar</p>
+                <Slider
+               aria-label="distance"
+               defaultValue={50}
+               valueLabelDisplay="auto"
+               step={10}
+               marks
+               min={0}
+               max={100}
+                />
+                <p>Word Frequency Scalar</p>
+                <Slider
+               aria-label="frequency"
+               defaultValue={50}
+               valueLabelDisplay="auto"
+               step={10}
+               marks
+               min={0}
+               max={100}
+                />
             </div>
             {    check &&
             <div className = "spell_check">
-                <p1>Cloest Match: </p1>
-                {
+                <p1>Cloest Match: </p1>   
+                <p1>{check_result}</p1>   
+                {  
+                    incorrect &&
                   matchlist.map(res => (<p key={res.word}>
-                    {res.word}: {res.dis}
+                    {res.word}: {res.dis} 
                   </p>))
                }
             </div>
@@ -130,6 +144,7 @@ export default function Main(){
                     inputProps = {{style: {fontSize: 20}}}
                     fullWidth 
                     margin = "normal"
+                    inputRef={dicRef}
                  />
               <Box m={2}>
                 <Button margin="20px" size = "large" variant = "contained"  onClick = {savedict}>Update Reference Dictionary</Button>
